cabc90a0eb941350e98af080e286870251d224d2
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-04-26 17:32:41 +0200

    init: server can be started now

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import morgan from "morgan";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.listen(49706, () => {
+	console.log("Listening on Port 49706");
+});
