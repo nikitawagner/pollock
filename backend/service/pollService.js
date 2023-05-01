a54d793daa9209a6f20d5a097026d44d0a8d742a
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-05-01 10:14:52 +0200

    temp: branching

--- backend/service/pollService.js
+++ backend/service/pollService.js
@@ -133,7 +133,108 @@ export const getPollLack = async (req, res, next) => {
 };
 
 export const putPollLack = async (req, res, next) => {
-	res.status(200).json({ message: "put" });
+	const { title, description, options, setting, fixed } = req.body;
+	const token = req.params.token;
+	const updatedPoll = {
+		title: title,
+		description: description,
+		fixed: fixed,
+	};
+	try {
+		const tokenResponse = await dbConnection.tokens.findFirst({
+			where: {
+				AND: {
+					value: token,
+					type: "admin",
+				},
+			},
+		});
+		if (tokenResponse) {
+			let pollId = tokenResponse.poll_fk;
+			await dbConnection.polls.update({
+				data: updatedPoll,
+				where: {
+					id: pollId,
+				},
+			});
+
+			await dbConnection.tokens.createMany({
+				data: [
+					{
+						value: v4(),
+						link: "string",
+						type: "admin",
+						poll_fk: pollId,
+					},
+					{
+						value: v4(),
+						link: "string",
+						type: "share",
+						poll_fk: pollId,
+					},
+				],
+			});
+
+			await Promise.all(
+				options.map(async (option) => {
+					await dbConnection.poll_options.create({
+						data: {
+							text: option.text,
+							polls: { connect: { id: pollId } },
+						},
+					});
+				})
+			);
+
+			await dbConnection.poll_settings.create({
+				data: {
+					voices: setting.voices,
+					worst: setting.worst,
+					deadline: setting.deadline,
+					polls: { connect: { id: pollId } },
+				},
+			});
+			const tokens = await dbConnection.tokens.findMany({
+				where: {
+					poll_fk: pollId,
+				},
+				orderBy: {
+					type: "asc",
+				},
+			});
+			let adminToken;
+			let shareToken;
+			tokens.forEach((token) => {
+				if (token.type === "admin") {
+					adminToken = token;
+				} else if (token.type === "share") {
+					shareToken = token;
+				}
+			});
+			const resultBody = {
+				admin: {
+					link: adminToken.link,
+					value: adminToken.value,
+				},
+				share: {
+					link: shareToken.link,
+					value: shareToken.value,
+				},
+			};
+			res.status(200).json(123);
+		} else {
+			res.status(404).json({
+				code: 404,
+				message: "Poll not found.",
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(404).json({
+			code: 404,
+			message: "Poll not found.",
+		});
+	}
 };
 
 export const deletePollLack = async (req, res, next) => {
