5f2915dfd79b75b6b1b19c1fe053745d343cfb63
tomek <t.banaszewski@ostfalia.de>
2023-05-04 17:50:20 +0200

    First try at Navbar.jsx. Also tried some styling

---
+++ pollock-frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => {
+    return (
+        <Navbar
+            bg="dark"
+            variant="dark"
+            expand="lg"
+            sticky="top"
+            style={{ width: "100%", zIndex: 1000 }}
+        >
+            <Navbar.Brand as={Link} to="/">
+                Poll App
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/create-poll">
+                        Create Poll
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/view-poll">
+                        View Poll
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default AppNavbar;
