f06ec549e6332979e21cb65cf69f44e7edbe020b
tomek <t.banaszewski@ostfalia.de>
2023-05-04 17:52:49 +0200

    - tried out routing and some design of the front page - Changed Projekts title on index.html - added bootstrap

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import pollRouter from "./router/pollRouter.js";
 import voteRouter from "./router/voteRouter.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- pollock-frontend/src/App.jsx
+++ pollock-frontend/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import CreatePoll from "./components/CreatePoll.jsx";
+import ViewPoll from "./components/ViewPoll.jsx";
+import UpdatePoll from "./components/UpdatePoll.jsx";
+import AppNavbar from "./components/Navbar.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+		<Router>
+			<div className="App">
+				<AppNavbar />
+				<header className="App-header">
+					<h1>Create a Poll</h1>
+				</header>
+				<main>
+					<Routes>
+						<Route path="/" element={<CreatePoll />} />
+						<Route path="/view-poll/:token" element={<ViewPoll />} />
+						<Route path="/update-poll/:adminToken" element={<UpdatePoll />} />
+					</Routes>
+				</main>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</Router>
 	);
 }
 
